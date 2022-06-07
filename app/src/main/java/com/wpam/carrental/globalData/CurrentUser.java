89d257562a97c21886ac71e224eb5f6b720bf0b5
Maciej <maciej.krysiak@softwaremind.com>
2022-06-07 16:29:26 +0200

    kod

--- app/src/main/java/com/wpam/carrental/globalData/CurrentUser.java
+++ app/src/main/java/com/wpam/carrental/globalData/CurrentUser.java
@@ -3,20 +3,11 @@ package com.wpam.carrental.globalData;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
-import com.wpam.carrental.model.Car;
 import com.wpam.carrental.model.enums.Role;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.Objects;
-
-import okhttp3.Call;
-import okhttp3.FormBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-import okhttp3.ResponseBody;
 
 public class CurrentUser {
 
